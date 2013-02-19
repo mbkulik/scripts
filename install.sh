903d56e23c77b880a4c5fdb357398733ed58c8ee
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-02-19 12:01:25 -0500

    add install-ruby

--- install.sh
+++ install.sh
@@ -4,5 +4,10 @@ if [ ! -d "$HOME/bin" ]; then
 	mkdir $HOME/bin
 fi
 
+
 cp update-ruby-environment $HOME/bin
+
 cp print-ruby-configure-args $HOME/bin
+
+cp install-ruby $HOME/bin
+cp install_ruby_completion.bash $HOME/bin
---
+++ install_ruby_completion.bash
@@ -0,0 +1,8 @@
+function _install_ruby_complete_()
+{
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	COMPREPLY=($(compgen -f ~/.rbenv/plugins/ruby-build/share/ruby-build/$cur | cut -b 55-))
+}
+
+complete -r install_ruby
+complete -o nospace  -X "*2.0.0*" -F _install_ruby_complete_ install_ruby
