f091d66c0d61ed2ea5da29117d64faf1dc811f7d
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-02-20 22:16:26 -0500

    cleanup some warnings and more flexibility with bash completion

--- install.sh
+++ install.sh
@@ -4,10 +4,16 @@ if [ ! -d "$HOME/bin" ]; then
 	mkdir $HOME/bin
 fi
 
-
 cp update-ruby-environment $HOME/bin
 
 cp print-ruby-configure-args $HOME/bin
 
 cp install-ruby $HOME/bin
-cp install_ruby_completion.bash $HOME/bin
+
+bash_completion=/usr/local/etc/bash_completion.d/
+
+if [ -d "$bash_completion" ]; then
+	cp install_ruby_completion.bash $bash_completion
+else
+	echo "needs bash-completion installed via HomeBrew"
+fi
--- install_ruby_completion.bash
+++ install_ruby_completion.bash
@@ -4,5 +4,4 @@ function _install_ruby_complete_()
 	COMPREPLY=($(compgen -f ~/.rbenv/plugins/ruby-build/share/ruby-build/$cur | cut -b 55-))
 }
 
-complete -r install-ruby
 complete -o nospace  -X "*2.0.0*" -F _install_ruby_complete_ install-ruby
