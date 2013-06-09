503cbcc484369ba6b5563e44f8fad98be029eeeb
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-06-09 16:08:35 -0400

    remove no longer used ruby-install script

--- install.sh
+++ install.sh
@@ -7,13 +7,3 @@ fi
 cp update-ruby-environment $HOME/bin
 
 cp print-ruby-configure-args $HOME/bin
-
-cp ruby-install $HOME/bin
-
-bash_completion=/usr/local/etc/bash_completion.d/
-
-if [ -d "$bash_completion" ]; then
-	cp ruby_install_completion.bash $bash_completion
-else
-	echo "needs bash-completion installed via HomeBrew"
-fi
